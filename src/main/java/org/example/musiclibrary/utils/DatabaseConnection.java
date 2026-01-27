5b9d07d70f244b19c688f183d1ff208c55854d97
sarife05 <sakenova.asima2007@gmail.com>
2026-01-27 16:30:22 +0500

    Update DatabaseConnection.java

--- src/main/java/org/example/musiclibrary/utils/DatabaseConnection.java
+++ src/main/java/org/example/musiclibrary/utils/DatabaseConnection.java
@@ -1,4 +1,16 @@
 package org.example.musiclibrary.utils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/musiclibrary";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "Hinata03";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
