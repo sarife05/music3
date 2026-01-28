fb359f78f424b00cd63e3f107d74fe62da57bd74
sarife05 <sakenova.asima2007@gmail.com>
2026-01-29 00:12:02 +0500

    PlaylistService

--- src/main/java/org/example/musiclibrary/service/PlaylistService.java
+++ src/main/java/org/example/musiclibrary/service/PlaylistService.java
@@ -1,4 +1,10 @@
 package org.example.musiclibrary.service;
 
+import org.example.musiclibrary.model.Media;
+import org.example.musiclibrary.model.Playlist;
+
 public class PlaylistService {
+    public void addMediaToPlaylist(Playlist playlist, Media media) {
+        playlist.addMedia(media);
+    }
 }
