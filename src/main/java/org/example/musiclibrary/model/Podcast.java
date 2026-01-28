0e43b9e883a27e50fe2f3fdb10d3c7468c457b1f
sarife05 <sakenova.asima2007@gmail.com>
2026-01-29 00:06:13 +0500

    Podcast.java
    Added Podcast class extending Media with host attribute and methods.

--- src/main/java/org/example/musiclibrary/model/Podcast.java
+++ src/main/java/org/example/musiclibrary/model/Podcast.java
@@ -1,4 +1,37 @@
 package org.example.musiclibrary.model;
 
-public class Podcast {
+public class Podcast extends Media {
+
+    private String host;
+
+    public Podcast(int id, String name, int duration, String host) {
+        super(id, name, duration);
+        this.name = name;
+        this.duration = duration;
+        this.host = host;
+    }
+
+    @Override
+    public String getType() {
+        return "PODCAST";
+    }
+
+    @Override
+    public String getCreator() {
+        return host;
+    }
+
+    @Override
+    public void play() {
+        System.out.println("Playing podcast: " + name);
+    }
+    @Override
+    public void validate() {
+        validateBase();   // проверка name и duration
+
+        if (host == null || host.isEmpty()) {
+            throw new IllegalArgumentException("Podcast host required");
+        }
+    }
+
 }
